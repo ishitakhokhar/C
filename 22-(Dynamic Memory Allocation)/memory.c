43b4404236b00df1609549b10b2eed2b7fab5765
ishitakhokhar <144801034+ishitakhokhar@users.noreply.github.com>
2024-06-30 11:24:29 +0000

    space removed

--- 22-(Dynamic Memory Allocation)/memory.c
+++ 22-(Dynamic Memory Allocation)/memory.c
@@ -1,4 +1,4 @@
-//  Allocate and de-allocate memory for int, char and float variable at run time.
+// Allocate and de-allocate memory for int, char and float variable at run time.
 #include <stdio.h>
 #include <stdlib.h>
 void main()
